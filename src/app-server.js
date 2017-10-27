c4346aa36b2161c3c996a51350cf19664f052c33
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-10-27 19:09:09 +0200

    Initial commit - now all files

---
+++ src/app-server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('winster').instance();
+const MongooseConnection = require('mongoose-connection-promise');
+const _ = require('lodash');
+const config = require('./config/config.js');
+
+class AppServer {
+
+  constructor() {
+    this.app = null;
+    this.server = null;
+    this.logger = null;
+    this.config = config;
+
+    this._initApp();
+
+  }
+
+  _initApp() {
+    this.app = express();
+    this.app.use(bodyParser.json());
+    this.logger = logger;
+  }
+
+  async start() {
+    this.server = await this.app.listen(this.config.PORT);
+  }
+
+  async stop() {
+    if (this.server) {
+      await this.server.close();
+    }
+  }
+
+
+}
+
+module.exports = AppServer;
